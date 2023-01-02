1e1973da450378a3ab3aac080c3dbb71f574648d
WilliamWJD <william.jose.wjd@gmail.com>
2023-01-02 10:04:57 -0300

    feat: configurando estilos globais

---
+++ src/components/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+    background-color: ${props => props.theme['green-500']};
+    border: none;
+    padding: 10px 20px;
+    margin-right: 10px;
+    color: #fff;
+    border-radius: 8px;
+    cursor: pointer;
+`;
