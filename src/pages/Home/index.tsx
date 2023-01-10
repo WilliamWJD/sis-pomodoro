cad183e4173754fc28ce97b97070832069dc0cb8
WilliamWJD <william.jose.wjd@gmail.com>
2023-01-10 08:45:41 -0300

    feat: iniciando novo ciclo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -23,7 +24,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
+interface Cycle {
+    id: string;
+    task: string;
+    minutes: number;
+}
+
 export function Home() {
+    const [cycles, setCycles] = useState<Cycle[]>([]);
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
     const { register, handleSubmit, watch, formState, reset } =
         useForm<NewCycleFormData>({
             resolver: zodResolver(newCycleFormValidationSchema),
@@ -34,14 +44,22 @@ export function Home() {
         });
 
     function handleCreateNewCycle(data: NewCycleFormData) {
-        console.log(data);
+        const newCycle: Cycle = {
+            id: String(new Date().getTime()),
+            task: data.task,
+            minutes: data.minutesAmount,
+        };
+
+        setCycles((state) => [...state, newCycle]);
+
+        setActiveCycleId(newCycle.id);
+
         reset();
     }
 
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
     const task = watch("task");
     const isSubmitDisabled = !task;
-
-    console.log(formState.errors);
 
     return (
         <HomeContainer>
